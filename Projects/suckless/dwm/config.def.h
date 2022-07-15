47c75eb3672115c511a4fcd6a20c8aa85c04d364
Hirschiii <niklasvonh@gmail.com>
2022-07-15 12:06:14 +0200

    added jmatrix to qutebrowser config

--- Projects/suckless/dwm/config.def.h
+++ Projects/suckless/dwm/config.def.h
@@ -77,7 +77,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", black, "-nf", green, "-sb", gray2, "-sf", red, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 #include <X11/XF86keysym.h>
 
