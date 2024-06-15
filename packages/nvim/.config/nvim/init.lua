16248afa44c1487206291c3d55889280e37fbd65
Niklas von Hirschfeld <niklas@vonhirschfeld.eu>
2024-06-15 18:22:36 +0200

    te

--- packages/nvim/.config/nvim/init.lua
+++ packages/nvim/.config/nvim/init.lua
@@ -9,15 +9,15 @@ require "user.keys"
 require "user.set"
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system {
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
+    "--branch=stable", -- latest stable release
     lazypath,
-  }
+  })
 end
 
 -- Add lazy to the `runtimepath`, this allows us to `require` it.
