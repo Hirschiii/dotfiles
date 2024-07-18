89be4d37eeabac4f3ed046fc616c12c6970e9087
Niklas von Hirschfeld <niklas@vonhirschfeld.eu>
2024-07-18 12:52:38 +0200

    lenovo screen :

--- packages/local/.local/bin/lenovo_screen.sh
+++ packages/local/.local/bin/lenovo_screen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 swaymsg -p output eDP-1 disable
-swaymsg -p output DP-1 scale 2
-swaymsg -p output HDMI-A-2 transform 270
+swaymsg -p output DP-1 pos 0 0 res 3840x2160 scale 2
+swaymsg -p output HDMI-A-2 pos 3840 0 transform 270
